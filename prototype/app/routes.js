4e28f4c5c39613635151102140cb40f03a5f4ef5
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-09-11 12:01:36 +0100

    Add prototyping code

---
+++ prototype/app/routes.js
@@ -0,0 +1,16 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+router.post('/nature-v1/main-applicant', function(request, response) {
+  console.log('IN MAIN-APPLICANT')
+  console.log(request.session.data)
+    
+  response.redirect("/nature-v1/task-list")
+})
