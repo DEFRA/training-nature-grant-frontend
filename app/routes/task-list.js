957c75a032b69c78dcec19cad5949c0704914460
Shet, Sandhya <Sandhya.Shet@defra.gov.uk>
2025-09-25 10:18:57 +0100

    Added a post method

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,7 +1,14 @@
-module.exports = {
+module.exports = [{
   method: 'GET',
   path: '/task-list',
   handler: (request, h) => {
     return h.view('task-list.html')
   }
-}
+},
+{
+  method: 'POST',
+  path: '/task-list',
+  handler: (request, h) => {
+    return h.redirect('/task-list')
+  }
+}]
