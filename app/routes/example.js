737d4c3da1fa0a741834fb750a2e4de31e9d07c4
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-10-02 08:41:29 +0100

    Render error message if validation error related to input

--- app/routes/example.js
+++ app/routes/example.js
@@ -5,27 +5,32 @@ module.exports = [
     method: 'GET',
     path: '/example',
     handler: (request, h) => {
-      const inputValue = request.yar.get('input-value')
-      return h.view('example.html', { inputValue })
+      const inputs = request.yar.get('input-values')
+      return h.view('example.html', inputs)
     }
   },
   {
     method: 'POST',
     path: '/example',
     handler: (request, h) => {
-      const { inputValue } = request.payload
-      request.yar.set('input-value', inputValue)
+      const inputs = request.payload
+      request.yar.set('input-values', inputs)
       return h.redirect('/task-list')
     },
     options: {
       validate: {
         payload: Joi.object({
-          inputValue: Joi.string().min(1).max(20)
+          inputValue: Joi.string().min(1).max(20),
+          inputValue2: Joi.string().min(1).max(20)
         }),
         failAction: async (request, h, error) => {
+          console.log(error.details[0])
           return h.view('example.html', {
             ...request.payload,
-            errorMessage: { text: 'Must be string between 1 and 20 characters' }
+            errorMessage: {
+              id: error.details[0].context.key,
+              text: 'Must be string between 1 and 20 characters'
+            }
           }).code(400).takeover()
         }
       }
