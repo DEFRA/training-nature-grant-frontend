e68f6ae444b30a61453c773bce305e150c7fd01c
Shet, Sandhya <Sandhya.Shet@defra.gov.uk>
2025-10-09 11:17:13 +0100

    Completed NT-5

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,11 +1,20 @@
+function getApplicantStatus (request) {
+  const firstName = request.yar.get('firstName')
+  const familyName = request.yar.get('familyName')
+  const email = request.yar.get('email')
+
+  const applicantstatus = firstName && familyName && email
+  const status = {
+    applicantDetails: applicantstatus
+  }
+  return status
+}
+
 module.exports = [{
   method: 'GET',
   path: '/task-list',
   handler: (request, h) => {
-    const firstName = request.yar.get('firstName')
-    const familyName = request.yar.get('familyName')
-    const email = request.yar.get('email')
-    const status = (firstName && familyName && email) ? 'Complete' : 'Incomplete'
+    const status = getApplicantStatus(request)
     return h.view('task-list.html', { status })
   }
 },
